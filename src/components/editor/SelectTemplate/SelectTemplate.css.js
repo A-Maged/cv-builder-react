c756baad880fa1cf20f7e8721dd3db645ecec7e9
a-maged <a.maged99@outlook.com>
2018-08-27 16:11:47 +0200

    .

--- src/components/editor/SelectTemplate/SelectTemplate.css.js
+++ src/components/editor/SelectTemplate/SelectTemplate.css.js
@@ -3,4 +3,11 @@ import { css } from 'emotion';
 export const selectTemplate = css`
    display: inline-block;
    padding: 15px;
+   h4 {
+      text-transform: uppercase;
+   }
+   img {
+      display: inline-block;
+      margin-bottom: 40px;
+   }
 `;
