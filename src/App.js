5f95d5c050da674e5bd80ca94c86efc42d15c8a6
a-maged <a.maged99@outlook.com>
2018-09-11 22:56:33 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
 
 /* COMPONENTS */
 import Routes from './Routes';
 import Nav from './components/Nav';
-import store from './redux/store';
 
 /* CSS */
 import './styles/bootstrap-grid-only-12.css';
@@ -15,17 +12,13 @@ import * as styles from './styles/App.css.js';
 class App extends Component {
    render() {
       return (
-         <Provider store={store}>
-            <Router>
-               <div className={styles.app}>
-                  <div className={styles.page}>
-                     <Routes />
-                  </div>
+         <div className={styles.app}>
+            <div className={styles.page}>
+               <Routes />
+            </div>
 
-                  <Nav />
-               </div>
-            </Router>
-         </Provider>
+            <Nav />
+         </div>
       );
    }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+   <Provider store={store}>
+      <Router>
+         <App />
+      </Router>
+   </Provider>,
+   document.getElementById('root')
+);
 registerServiceWorker();
